3ec0b736a0223d2be5a0e9a72483ba84ed80bf80
Vincent Abolarin <vincentabolarin@gmail.com>
2022-04-29 16:40:29 +0100

    set up app,header, button, tasks and task-item

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Appointment with the Doctor',
+        day: '1st May',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: '2nd May',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: '3rd May',
+        reminder: false
+    }
+];
